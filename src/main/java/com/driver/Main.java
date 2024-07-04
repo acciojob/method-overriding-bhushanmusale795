e5b030d714753d3512f8ec6772c3ab825cd463e6
Bhushan <bhushanmusale795@outlook.com>
2024-07-04 19:06:02 +0530

    hey

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,7 +17,7 @@ public class Main {
         // Override the meth method from class A
         @Override
         public String meth() {
-            return "Method is overridden in Extended class B";
+            return "Method is overridden in Extendend class B";
         }
     }
     public static void main(String[] args) {
