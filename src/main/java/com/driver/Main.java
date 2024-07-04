c964831d5360adc91eb601f56d3e191c3f4cbb06
Bhushan <bhushanmusale795@outlook.com>
2024-07-04 19:02:27 +0530

    hey

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,24 +2,24 @@ package com.driver;
 
 
 // Define the superclass A
-class A {
-    // Method meth that returns a String
-    public String meth() {
-        return "Invoking method from class A";
-    }
-}
-
-// Define the subclass B which extends A
-class B extends A {
-    // Override the meth method from class A
-    @Override
-    public String meth() {
-        return "Method is overridden in Extended class B";
-    }
-}
 
 // Main class to test the functionality
 public class Main {
+   static class A {
+        // Method meth that returns a String
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    // Define the subclass B which extends A
+   static class B extends A {
+        // Override the meth method from class A
+        @Override
+        public String meth() {
+            return "Method is overridden in Extended class B";
+        }
+    }
     public static void main(String[] args) {
         // Create an object of subclass B
         B objB = new B();
@@ -30,6 +30,8 @@ public class Main {
 
         // Call the meth method from class A using objB
         String resultA = objB.meth();  // Calls overridden method in B
-        System.out.println(resultA);  // Output: "Method is overridden in Extended class B"
+        System.out.println(resultA);
+        // Output: "Method is overridden in Extended class B"
     }
+
 }
